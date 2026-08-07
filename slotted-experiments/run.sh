a4074cad05b4c934af325dfb13e6b9515cd8ccff
memoryleak47 <memoryleak47@protonmail.ch>
2026-08-07 11:54:12 +0200

    ...

--- slotted-experiments/run.sh
+++ slotted-experiments/run.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
+pat="$1"
+
 for x in $(find tests -type f | sort)
 do
+    if [[ ! "$x" == *"$pat"* ]]; then
+        continue
+    fi
     echo "Test: $x"
     cat preamble-compiled.egg > output.egg
     ./transform.py "$x" >> output.egg
